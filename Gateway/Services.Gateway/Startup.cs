36fe8b0072a3e06b8a130587e0e1623b48e7d7b3
MeralTd <meraltasdemir77@gmail.com>
2021-06-23 17:09:17 +0300

    Add Ocelot package

--- Gateway/Services.Gateway/Startup.cs
+++ Gateway/Services.Gateway/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,25 +18,27 @@ namespace Services.Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddHttpClient<TokenExhangeDelegateHandler>();
+            //services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
+            //{
+            //    options.Authority = Configuration["IdentityServerURL"];
+            //    options.Audience = "resource_gateway";
+            //    options.RequireHttpsMetadata = false;
+            //});
+
+            services.AddOcelot()/*.AddDelegatingHandler<TokenExhangeDelegateHandler>();*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        async public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
+            await app.UseOcelot();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
